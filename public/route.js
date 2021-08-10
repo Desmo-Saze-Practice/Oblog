d67396c858d6d34887021b231c1681e1a304a9cc
florian <martinflorian2@gmail.com>
2021-08-10 08:23:39 +0200

    challenge over, added dynamic route for articles and dynamic views. Also 404 handled

--- public/route.js
+++ public/route.js
@@ -1,21 +1,37 @@
 const express = require('express');
 
-const posts = require('../data/posts.json');
+const postsData = require('../data/posts.json');
 
 const router = express.Router();
 
-router.use('',(req, res, next) => {
-    res.locals.posts = posts;
+router.use((req, res, next) => {
+    res.locals.posts = postsData;
     next();
 })
 
 router.get('/', (req, res) => {
-    res.render('./index.ejs');
+    res.render('index');
 });
 
-router.get('/post/id:', (req, res) => {
-    let postId = req.params
-    console.log(postId); 
+router.get('/post/:id', (req, res) => {
+    let postId = req.params.id;
+    
+    const currentPost = postsData.find(post => post.id.toString() === postId.toString());
+
+    if (!currentPost) {
+        res.status(404).render('404');
+        return;
+    }
+
+    res.locals.foundPost = currentPost;
+
+    res.render('post', currentPost);
+
+
 });
+
+router.use((req, res) => {
+    res.status(404).render('404');
+})
 
 module.exports = router;
